7c8c6f0b7bb698109e0313b03a401d4710bc4fc8
Henry Lin <henrylin58@gmail.com>
2019-12-16 12:43:46 -0800

    adding variables to response

--- nurtrition.js
+++ nurtrition.js
@@ -42,7 +42,24 @@ function foodItem() {
             }
         }).then(function(response) {
             console.log(response);
-            console.log(post)
+            var calories = response.foods[0].nf_calories;
+            var calfat = (9 * response.foods[0].nf_total_fat);
+            var fatpercent = ((response.foods[0].nf_total_fat / 65) * 100);
+            var satfat = response.foods[0].nf_saturated_fat;
+            var transfat = response.foods[0].full_nutrients[53].value;
+            var cholesterol = response.foods[0].nf_cholesterol;
+            var cholesterolpercent = ((response.foods[0].nf_calories / 300) * 100);
+            var sodium = response.foods[0].nf_sodium;
+            var totalcarbs = response.foods[0].nf_total_carbohydrate
+            var carbpercent = ((response.foods[0].nf_total_carbohydrate / 300) * 100);
+            var fiber = response.foods[0].nf_dietary_fiber;
+            var sugar = response.foods[0].nf_sugars;
+            var protein = response.foods[0].nf_protein;
+            var potassium = response.foods[0].nf_potassium;
+            var vita = ((response.foods[0].full_nutrients[30].value) / 5000) * 100;
+            var vitc = ((response.foods[0].full_nutrients[40].value) / 60) * 100;
+            var calcium = ((response.foods[0].full_nutrients[19].value) / 1300) * 100;
+            var iron = ((response.foods[0].full_nutrients[20].value) / 18) * 100;
         })
 
     })
